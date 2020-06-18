510efe681993d1db5f409e49ba8e9bbdf871bcdd
Regis Albuquerque <regis.albuquerque1@gmail.com>
2020-06-18 15:02:28 -0500

    Minimal Spring Boot Project generated by Spring Initializr website tool

---
+++ src/main/java/regisalbuquerque/minimalspring/MinimalSpringApplication.java
@@ -0,0 +1,13 @@
+package regisalbuquerque.minimalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/regisalbuquerque/minimalspring/MinimalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package regisalbuquerque.minimalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
